5c9724e19a50c74b4b9dcd28b21ec6c8429812cc
Charlotte Curtis <ccurtis@mtroyal.ca>
2021-11-05 11:50:33 -0600

    Updated repo grading script to randomize order and open PR changes if deadline is blank

--- grade_repos.sh
+++ grade_repos.sh
@@ -10,23 +10,33 @@ fi
 
 read -p "What is the assignment deadline? (format yyyy-mm-dd) " deadline
 
-for repo in */; do
+for repo in `ls -d */ | shuf`; do
+	if [ -f "$repo/feedback.md" ]; then
+		# We've already done this one, skip to the next
+		echo "Already marked $repo, skipping"
+		continue
+	fi
 	COUNTER=$[$COUNTER + 1]
 	if [ $COUNTER -ge $START ]; then
 		echo "$COUNTER: $repo"
 		cd $repo
 
-		# get the hash associated with the deadline (up to midnight)
-		sha_deadline=`git log --until="$deadline 23:59" --format=format:%H -1`
-
-		# and the initial commit
-		sha_init=`git rev-list HEAD | tail -n 1`
-
-		# and the base URL without the .git part
+		# get the base URL without the .git part
 		url=`git config --get remote.origin.url`
-		
-		# compare the code at the deadline to the initial commit
-		start "${url%.*}/compare/$sha_init...$sha_deadline"
+
+		if [ -z "$deadline" ]; then
+			# just open the feedback pr to the files view
+			start "${url%.*}/pull/1/files"
+		else
+			# get the hash associated with the deadline (up to midnight)
+			sha_deadline=`git log --until="$deadline 23:59" --format=format:%H -1`
+
+			# and the initial commit
+			sha_init=`git rev-list HEAD | tail -n 1`
+			
+			# compare the code at the deadline to the initial commit
+			start "${url%.*}/compare/$sha_init...$sha_deadline"
+		fi
 
 		# Open the text file for feedback
 		code -w feedback.md
