35f07a2504312a111bea73e6087e1d9a11bc3801
Charlotte Curtis <ccurtis@mtroyal.ca>
2022-10-06 17:31:22 -0600

    Updating run_scripts to take optional user inputs

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,14 +1,26 @@
 #!/bin/bash
 
+# usage: ./run_scripts.sh base_dir script_name [user_inputs.txt]
+# where:
+#   base_dir is the directory containing all student repose
+#   script_name is the Python script to run (including .py)
+#   user_inputs.txt is an optional file containing user inputs to the script, one per line
+
 cd $1
+SCRIPT_NAME=$2
+
+if [[ "$#" -gt 2 ]]; then
+	INPUT_FILE="`pwd`/$3"
+else
+    INPUT_FILE=/dev/null
+fi
+
 
 for repo in `ls -d */`; do
     cd $repo
     echo $repo
-    if ls *.py 1> /dev/null 2>&1; then
-        python *.py
-    elif  ls *.txt 1> /dev/null 2>&1; then
-        python *.txt
+    if ls "$SCRIPT_NAME" 1> /dev/null 2>&1; then
+        python "$SCRIPT_NAME" < "$INPUT_FILE"
     else
         echo "*** Missing Python File ***"
     fi
