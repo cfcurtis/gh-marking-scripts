1a4949ad7f5b419cd62a2ee9614f296c37a5e1cf
Charlotte Curtis <ccurtis@mtroyal.ca>
2023-01-19 17:08:08 -0700

    Making autograde script work with pytest

--- test_to_autograde.py
+++ test_to_autograde.py
@@ -1,6 +1,8 @@
 import unittest
+from unittest.mock import patch
+from io import StringIO
 import sys
-import os
+from pathlib import Path
 import json
 
 def flatten_tests(tests):
@@ -16,17 +18,58 @@ def flatten_tests(tests):
 
     return flat_list
 
+def parse_unittests(tests):
+    """
+    Converts the list of test cases from unittest into description/name pairs.
+    """
+    test_list = []
+    for test in tests:
+        test_list.append({"description": test.shortDescription(), "command": f"python3 -m unittest {test.id()}"})
+
+    return test_list
+
+def parse_pytests(tests):
+    """
+    Converts the list of test cases from pytest into description/name pairs.
+    """
+    test_list = []
+    for test in tests:
+        path, test_func = test.split("::")
+        test_file = Path(path).name
+        test_list.append({"description": test_func, "command": f"pytest{test_file}::{test_func}"})
+
+    return test_list
+
 def get_tests(folder):
     """
     Returns a list of all the test methods in a folder.
     """
+    # try unittests first
     test_list = []
     for test in unittest.TestLoader().discover(folder):
         for test_case in test:
             test_list.append(test_case)
     
-    # flatten the list
-    return flatten_tests(test_list)
+    if len(test_list) > 0:
+        return parse_unittests(flatten_tests(test_list))
+    else:
+        try:
+            import pytest
+        except ImportError:
+            print("No unittests found and pytest not installed, exiting.")
+            sys.exit()
+
+        # get the list of tests from pytest. Pretty hacky.
+        # Capture stdout, then run pytest.main
+        stream = StringIO()
+        with patch('sys.stdout', new=stream):
+            pytest.main(['-q', folder, '--collect-only'])
+            for line in stream.getvalue().splitlines():
+                if "::" in line:
+                    test_list.append(line)
+        
+        return parse_pytests(test_list)
+
 
 def main():
     folder = sys.argv[1]
@@ -34,9 +77,9 @@ def main():
     autograding = {'tests': []}
     for test in tests:
         test_case = {
-            'name': test.shortDescription(),
+            'name': test["description"],
             "setup": "",
-            "run": "python3 -m unittest " + test.id(),
+            "run": test["command"],
             "input": "",
             "output": "",
             "comparison": "included",
@@ -46,14 +89,14 @@ def main():
         autograding['tests'].append(test_case)
     
     # create the .github folder if it doesn't already exist
-    gh_folder = os.path.join(folder, '.github')
-    if not os.path.exists(gh_folder):
-        os.mkdir(gh_folder)
-    classroom_folder = os.path.join(gh_folder, 'classroom')
-    if not os.path.exists(classroom_folder):
-        os.mkdir(classroom_folder)
+    gh_folder = Path(folder) / '.github'
+    if not gh_folder.exists():
+        gh_folder.mkdir()
+    classroom_folder = gh_folder / 'classroom'
+    if not classroom_folder.exists():
+        classroom_folder.mkdir()
     
-    with open(os.path.join(classroom_folder, 'autograding.json'), 'w') as f:
+    with open(classroom_folder / 'autograding.json', 'w') as f:
         json.dump(autograding, f, indent=2)
 
 if __name__ == '__main__':
